7b4a15a3854cf08a8f6585349d92da667db5260d
PruthviKatna <pruthvi.katna@gmail.com>
2021-07-07 23:14:10 +0530

    CodeChefLc1

---
+++ LongChallenges/July2021/MaximumProductivity.cpp
@@ -0,0 +1,11 @@
+    #include<bits/stdc++.h>
+    using namespace std ;
+    int main (){
+        int t ;
+        cin>>t;
+        while(t--){
+            int d, x, y, z;
+            cin >>d >> x >> y >> z;
+            cout << max(x*7 , (d*y+((7-d)*z))) <<endl ;
+        }
+    }
---
+++ LongChallenges/July2021/Relativity.cpp
@@ -0,0 +1,11 @@
+    #include<bits/stdc++.h>
+    using namespace std ;
+    int main (){
+        int t ;
+        cin>>t;
+        while(t--){
+            int c,g;
+            cin >>g>>c;
+            cout<<(c*c/(2*g))<<endl;  
+        }
+    }
